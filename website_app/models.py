7fed0ce5a6fc1f984a877dd01851ef92f4fe8eb1
RickWeber <863114+RickWeber@users.noreply.github.com>
2022-06-20 10:27:19 -0400

    flesh out basic logic

--- website_app/models.py
+++ website_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+class Service(models.Model):
+    short_name = models.CharField(max_length=25)
+    description = RichTextField()
+
+class Resource(models.Model):
+    short_name = models.CharField(max_length=25)
+    description = RichTextField()
+    url = models.URLField()
+
+class PaymentOption(models.Model):
+    """A class to hold information on insurance providers, cash prices, etc."""
+    pass
+
+class Counselor(models.Model):
+    """A class to hold data and methods about Dana"""
+    first_name = models.CharField(default="Dana", max_length=35)
+    last_name = models.CharField(default="Ellis", max_length=35)
+    phone_number = models.CharField(default="631-255-5020", max_length=15)
+    email = models.EmailField()
+    def __str__(self):
+        return f"{first_name} {last_name}"
